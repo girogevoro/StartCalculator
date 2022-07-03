a8bc4e4dff8a7bd3a1184fccfff717cc979f8501
egorovin <egorovin@mail.ru>
2022-07-03 23:09:31 +0300

    lesson5

--- app/src/main/java/com/girogevoro/startcalculator/MainActivity.java
+++ app/src/main/java/com/girogevoro/startcalculator/MainActivity.java
@@ -2,13 +2,28 @@ package com.girogevoro.startcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DATA = "data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.start).setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("calculator://start"));
+            intent.putExtra(DATA, getString(R.string.start_calculator));
+
+            Intent choice_calculator = Intent.createChooser(intent, getString(R.string.choice_calculator));
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(choice_calculator);
+            }
+
+        });
     }
 }
